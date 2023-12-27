7979b57bc410b29d3af313d267cf69c6e62c9e30
bizhan <bizhan.laripour.herat@gmail.com>
2023-12-27 13:54:12 +0330

    final commit for Agent modul

--- src/main/java/com/submodule/kafka/Consumer.java
+++ src/main/java/com/submodule/kafka/Consumer.java
@@ -14,33 +14,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class Consumer {
 
-    @KafkaListener(topics = "WORKER_ONE", containerFactory = "kafkaListenerContainerFactory")
-    public void consumeWorkerOne(ConsumerRecord<String, ConsumerDto> review) {
-        ConsumerDto consumerDto = review.value();
-        Gson gson = new Gson();
-        String thresholdString = gson.toJson(consumerDto.getObject());
-        Threshold threshold = gson.fromJson(thresholdString, Threshold.class);
-        System.out.println("threshold received from worker 1 that is:");
-        System.out.println(threshold);
-    }
-
-    @KafkaListener(topics = "WORKER_TWO", containerFactory = "kafkaListenerContainerFactory")
-    public void consumeWorkerTwo(ConsumerRecord<String, ConsumerDto> review) {
-        ConsumerDto consumerDto = review.value();
-        Gson gson = new Gson();
-        String thresholdString = gson.toJson(consumerDto.getObject());
-        Threshold threshold = gson.fromJson(thresholdString, Threshold.class);
-        System.out.println("threshold received from worker 2 that is:");
-        System.out.println(threshold);
-    }
-
-    @KafkaListener(topics = "WORKER_THREE", containerFactory = "kafkaListenerContainerFactory")
-    public void consumeWorkerThree(ConsumerRecord<String, ConsumerDto> review) {
-        ConsumerDto consumerDto = review.value();
-        Gson gson = new Gson();
-        String thresholdString = gson.toJson(consumerDto.getObject());
-        Threshold threshold = gson.fromJson(thresholdString, Threshold.class);
-        System.out.println("threshold received from worker 3 that is:");
-        System.out.println(threshold);
-    }
+//    @KafkaListener(topics = "WORKER_ONE", containerFactory = "kafkaListenerContainerFactory")
+//    public void consumeWorkerOne(ConsumerRecord<String, ConsumerDto> review) {
+//        ConsumerDto consumerDto = review.value();
+//        Gson gson = new Gson();
+//        String thresholdString = gson.toJson(consumerDto.getObject());
+//        Threshold threshold = gson.fromJson(thresholdString, Threshold.class);
+//        System.out.println("threshold received from worker 1 that is:");
+//        System.out.println(threshold);
+//    }
+//
+//    @KafkaListener(topics = "WORKER_TWO", containerFactory = "kafkaListenerContainerFactory")
+//    public void consumeWorkerTwo(ConsumerRecord<String, ConsumerDto> review) {
+//        ConsumerDto consumerDto = review.value();
+//        Gson gson = new Gson();
+//        String thresholdString = gson.toJson(consumerDto.getObject());
+//        Threshold threshold = gson.fromJson(thresholdString, Threshold.class);
+//        System.out.println("threshold received from worker 2 that is:");
+//        System.out.println(threshold);
+//    }
+//
+//    @KafkaListener(topics = "WORKER_THREE", containerFactory = "kafkaListenerContainerFactory")
+//    public void consumeWorkerThree(ConsumerRecord<String, ConsumerDto> review) {
+//        ConsumerDto consumerDto = review.value();
+//        Gson gson = new Gson();
+//        String thresholdString = gson.toJson(consumerDto.getObject());
+//        Threshold threshold = gson.fromJson(thresholdString, Threshold.class);
+//        System.out.println("threshold received from worker 3 that is:");
+//        System.out.println(threshold);
+//    }
 }
