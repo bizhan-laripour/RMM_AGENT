f2273365b21840da4c80e4c9a2f37351953bfc24
bizhan <bizhan.laripour.herat@gmail.com>
2024-01-20 10:10:54 +0330

    final commit for Agent module

--- src/main/java/com/submodule/scheduler/WorkerScheduler.java
+++ src/main/java/com/submodule/scheduler/WorkerScheduler.java
@@ -1,6 +1,7 @@
 package com.submodule.scheduler;
 
 import com.submodule.dto.ZabbixResponseDto;
+import com.submodule.dto.ZabbixResultItemDto;
 import com.submodule.entity.Threshold;
 import com.submodule.feign.WorkerFeign;
 import com.submodule.service.ThresholdService;
@@ -27,7 +28,7 @@ public class WorkerScheduler {
     }
 
     @Scheduled(fixedRate = 60000)
-    public void getDeviceInformationFromElasticSearch(){
+    public void getDeviceInformationFromElasticSearch() throws Exception {
         ZabbixResponseDto zabbixResponseDto = workerFeign.getFromWorker(environment.getProperty("agent.ip"));
         List<Threshold> thresholds = thresholdService.findByIp(environment.getProperty("agent.ip"));
         for(Threshold threshold: thresholds){
@@ -37,7 +38,18 @@ public class WorkerScheduler {
     }
 
 
-    public void compareAndCheck(ZabbixResponseDto zabbixResponseDto , Threshold threshold){
+    public void compareAndCheck(ZabbixResponseDto zabbixResponseDto , Threshold threshold) throws Exception {
+        switch (threshold.getCategory()){
+            case MEMORY_USAGE : checkMemory(zabbixResponseDto , threshold);
+        }
+    }
 
+    private void checkMemory(ZabbixResponseDto zabbixResponseDto , Threshold threshold) throws Exception {
+        List<ZabbixResultItemDto> result = zabbixResponseDto.getResult().stream().filter(obj -> obj.getItemid().equals("37412")).toList();
+        if(result != null && !result.isEmpty()){
+            if(threshold.getPercentage() < Long.valueOf(result.get(0).getLastvalue())){
+                throw new Exception("memory is under attack");
+            }
+        }
     }
 }
