f9a866d8304906a42c510c01a00d9bf1072fb2c6
Bizhan <bizhan.laripour.herat@gmail.com>
2024-05-16 19:58:31 -0400

    change feign

--- src/main/java/com/submodule/feign/WorkerFeign.java
+++ src/main/java/com/submodule/feign/WorkerFeign.java
@@ -5,9 +5,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @FeignClient(name = "worker")
 public interface WorkerFeign {
     @PostMapping(value = "/find-by-ip")
     ZabbixResponseDto getFromWorker(@RequestParam(name="ip") String ip);
+
+    @PostMapping(value = "/find-all-by-ip")
+    List<ZabbixResponseDto> getAllFromWorker(@RequestParam(name = "ip") String ip);
 
 }
