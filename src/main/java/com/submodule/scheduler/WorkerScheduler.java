3f4e47531f2bd7b0590cdaa0ca145a9a728fed31
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-05-16 19:55:10 -0400

    Update WorkerScheduler.java

--- src/main/java/com/submodule/scheduler/WorkerScheduler.java
+++ src/main/java/com/submodule/scheduler/WorkerScheduler.java
@@ -16,8 +16,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 @Component
@@ -44,14 +48,15 @@ public class WorkerScheduler {
     }
 
     @Scheduled(fixedRate = 60000)
-    public void getDeviceInformationFromElasticSearch(){
+    public void getDeviceInformationFromElasticSearch() {
 
         List<Threshold> thresholds = thresholdService.findByIp(environment.getProperty("agent.ip"));
         if (!thresholds.isEmpty()) {
             ZabbixResponseDto zabbixResponseDto = workerFeign.getFromWorker(environment.getProperty("agent.ip"));
+            List<ZabbixResponseDto> zabbixResponseDtoList = workerFeign.getAllFromWorker(environment.getProperty("agent.ip"));
             List<Alarm> alarms = new ArrayList<>();
             for (Threshold threshold : thresholds) {
-                Alarm alarm = compareAndCheck(zabbixResponseDto, threshold);
+                Alarm alarm = compareAndCheck(zabbixResponseDto, zabbixResponseDtoList, threshold);
                 if (alarm != null) {
                     alarms.add(alarm);
                     alarmService.save(alarm);
@@ -73,7 +78,7 @@ public class WorkerScheduler {
         List<Threshold> thresholds = thresholdService.findByIp(environment.getProperty("agent.ip"));
         List<Alarm> alarms = alarmService.findByIp(environment.getProperty("agent.ip"));
         ZabbixResponseDto zabbixResponseDto = workerFeign.getFromWorker(environment.getProperty("agent.ip"));
-        alarms.stream().filter(Alarm::getActive).forEach(alarm ->checkActiveAlarms(alarm , thresholds , zabbixResponseDto));
+        alarms.stream().filter(Alarm::getActive).forEach(alarm -> checkActiveAlarms(alarm, thresholds, zabbixResponseDto));
     }
 
     private void checkActiveAlarms(Alarm alarm, List<Threshold> thresholds, ZabbixResponseDto zabbixResponseDto) {
@@ -84,14 +89,14 @@ public class WorkerScheduler {
                 alarm.setActive(false);
                 alarmService.save(alarm);
             }
-        }else if(alarm.getCategory().equals(Category.CPU_LOAD)){
+        } else if (alarm.getCategory().equals(Category.CPU_LOAD)) {
             List<Threshold> t = thresholds.stream().filter(threshold -> threshold.getCategory().equals(Category.CPU_LOAD)).toList();
             List<ZabbixResultItemDto> cpu = zabbixResponseDto.getResult().stream().filter(obj -> obj.getItemid().equals("37415")).toList();
             if (t.get(0).getPercentage() > Double.parseDouble(cpu.get(0).getLastvalue())) {
                 alarm.setActive(false);
                 alarmService.save(alarm);
             }
-        }else if(alarm.getCategory().equals(Category.HARD_DISK_USAGE)){
+        } else if (alarm.getCategory().equals(Category.HARD_DISK_USAGE)) {
             List<Threshold> t = thresholds.stream().filter(threshold -> threshold.getCategory().equals(Category.HARD_DISK_USAGE)).toList();
             List<ZabbixResultItemDto> hard = zabbixResponseDto.getResult().stream().filter(obj -> obj.getItemid().equals("37608")).toList();
             if (t.get(0).getPercentage() > Double.parseDouble(hard.get(0).getLastvalue())) {
@@ -102,68 +107,117 @@ public class WorkerScheduler {
     }
 
 
-    public Alarm compareAndCheck(ZabbixResponseDto zabbixResponseDto, Threshold threshold) {
+    public Alarm compareAndCheck(ZabbixResponseDto zabbixResponseDto, List<ZabbixResponseDto> zabbixResponseDtoList, Threshold threshold) {
         switch (threshold.getCategory()) {
             case MEMORY_USAGE -> {
-                return checkMemory(zabbixResponseDto, threshold);
+                return checkMemory(zabbixResponseDto, zabbixResponseDtoList, threshold);
             }
             case HARD_DISK_USAGE -> {
-                return checkHard(zabbixResponseDto, threshold);
+                return checkHard(zabbixResponseDto, zabbixResponseDtoList, threshold);
             }
             case CPU_LOAD -> {
-                return checkCpu(zabbixResponseDto, threshold);
+                return checkCpu(zabbixResponseDto, zabbixResponseDtoList, threshold);
             }
         }
         return null;
     }
 
-    private Alarm checkMemory(ZabbixResponseDto zabbixResponseDto, Threshold threshold) {
+    private Alarm checkMemory(ZabbixResponseDto zabbixResponseDto, List<ZabbixResponseDto> zabbixResponseDtoList, Threshold threshold) {
         List<ZabbixResultItemDto> memory = zabbixResponseDto.getResult().stream().filter(obj -> obj.getItemid().equals("37412")).toList();
         if (!memory.isEmpty()) {
             if (threshold.getPercentage() <= Double.parseDouble(memory.get(0).getLastvalue())) {
-                Alarm alarm = new Alarm();
-                alarm.setCategory(threshold.getCategory());
-                alarm.setDate(new Date());
-                alarm.setIp(threshold.getIp());
-                alarm.setThresholdUUID(threshold.getThresholdUUID());
-                //this is available memory
-                alarm.setMemoryUsage(Double.parseDouble(zabbixResponseDto.getResult().stream().filter(obj -> obj.getItemid().equals("37411")).toList().get(0).getLastvalue()));
-                return alarm;
+                List checkTime = checkTime(threshold, zabbixResponseDtoList, zabbixResponseDto, "37412");
+                if ((Boolean) checkTime.get(0)) {
+                    Alarm alarm = new Alarm();
+                    alarm.setCategory(threshold.getCategory());
+                    alarm.setTimePeriodInSec(checkTime.get(1).toString());
+                    alarm.setDate(new Date());
+                    alarm.setIp(threshold.getIp());
+                    alarm.setThresholdUUID(threshold.getThresholdUUID());
+                    //this is available memory
+                    alarm.setMemoryUsage(Double.parseDouble(zabbixResponseDto.getResult().stream().filter(obj -> obj.getItemid().equals("37411")).toList().get(0).getLastvalue()));
+                    return alarm;
+                }
             }
         }
         return null;
     }
 
-    private Alarm checkHard(ZabbixResponseDto zabbixResponseDto, Threshold threshold){
+    private Alarm checkHard(ZabbixResponseDto zabbixResponseDto, List<ZabbixResponseDto> zabbixResponseDtoList, Threshold threshold) {
         List<ZabbixResultItemDto> spaceUtilization = zabbixResponseDto.getResult().stream().filter(obj -> obj.getItemid().equals("37608")).toList();
         if (!spaceUtilization.isEmpty()) {
             if (threshold.getPercentage() <= Double.parseDouble(spaceUtilization.get(0).getLastvalue())) {
-                Alarm alarm = new Alarm();
-                alarm.setCategory(threshold.getCategory());
-                alarm.setIp(threshold.getIp());
-                alarm.setDate(new Date());
-                alarm.setThresholdUUID(threshold.getThresholdUUID());
-                alarm.setMemoryUsage(Double.parseDouble(spaceUtilization.get(0).getLastvalue()));
-                return alarm;
+                List checkTime = checkTime(threshold, zabbixResponseDtoList, zabbixResponseDto, "37608");
+                if ((Boolean) checkTime.get(0)) {
+                    Alarm alarm = new Alarm();
+                    alarm.setCategory(threshold.getCategory());
+                    alarm.setTimePeriodInSec(checkTime.get(1).toString());
+                    alarm.setIp(threshold.getIp());
+                    alarm.setDate(new Date());
+                    alarm.setThresholdUUID(threshold.getThresholdUUID());
+                    alarm.setMemoryUsage(Double.parseDouble(spaceUtilization.get(0).getLastvalue()));
+                    return alarm;
+                }
             }
         }
         return null;
     }
 
-    private Alarm checkCpu(ZabbixResponseDto zabbixResponseDto, Threshold threshold) {
+    private Alarm checkCpu(ZabbixResponseDto zabbixResponseDto, List<ZabbixResponseDto> zabbixResponseDtoList, Threshold threshold) {
         List<ZabbixResultItemDto> cpu = zabbixResponseDto.getResult().stream().filter(obj -> obj.getItemid().equals("37415")).toList();
         if (!cpu.isEmpty()) {
             if (threshold.getPercentage() <= Double.parseDouble(cpu.get(0).getLastvalue())) {
-                Alarm alarm = new Alarm();
-                alarm.setCategory(threshold.getCategory());
-                alarm.setIp(threshold.getIp());
-                alarm.setDate(new Date());
-                alarm.setThresholdUUID(threshold.getThresholdUUID());
-                alarm.setCpuLoad(Double.parseDouble(cpu.get(0).getLastvalue()));
-                return alarm;
+                List checkTime = checkTime(threshold, zabbixResponseDtoList, zabbixResponseDto, "37415");
+                if ((Boolean) checkTime.get(0)) {
+                    Alarm alarm = new Alarm();
+                    alarm.setCategory(threshold.getCategory());
+                    alarm.setTimePeriodInSec(checkTime.get(1).toString());
+                    alarm.setIp(threshold.getIp());
+                    alarm.setDate(new Date());
+                    alarm.setThresholdUUID(threshold.getThresholdUUID());
+                    alarm.setCpuLoad(Double.parseDouble(cpu.get(0).getLastvalue()));
+                    return alarm;
+                }
             }
         }
         return null;
+    }
+
+    private List checkTime(Threshold threshold, List<ZabbixResponseDto> zabbixResponseDtoList, ZabbixResponseDto lastOne, String itemId) {
+        AtomicBoolean lastResult = new AtomicBoolean(false);
+        AtomicBoolean breakFromLoop = new AtomicBoolean(false);
+        AtomicLong atomicLong = new AtomicLong(0);
+        zabbixResponseDtoList.forEach(zabbixResponseDto -> {
+            if (!lastResult.get() && !breakFromLoop.get()) {
+                List<ZabbixResultItemDto> result = zabbixResponseDto.getResult().stream().filter(obj -> obj.getItemid().equals(itemId)).toList();
+                if (threshold.getPercentage() <= Double.parseDouble(result.get(0).getLastvalue())) {
+                    Long seconds = calculateSecondsBetweenDates(zabbixResponseDto.getDate(), lastOne.getDate());
+                    if (threshold.getTimePeriod() <= seconds) {
+                        lastResult.set(true);
+                        atomicLong.set(seconds);
+                    }
+                } else {
+                    breakFromLoop.set(true);
+                }
+            }
+        });
+        List list = new ArrayList();
+        list.add(lastResult.get());
+        list.add(atomicLong.get());
+
+        return list;
+    }
+
+
+    private Long calculateSecondsBetweenDates(Date date1, Date date2) {
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTime(date1);
+        cal2.setTime(date2);
+        long diff = cal2.getTimeInMillis() - cal1.getTimeInMillis();
+        // convert the time difference to seconds
+        long diffSeconds = diff / 1000;
+        return diffSeconds;
     }
 
 }
